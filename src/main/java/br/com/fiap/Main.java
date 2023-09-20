f397aa5e349ec4f9bbc9e1b6be3f501b4d219a0a
Natan-333 <natancruz23@gmail.com>
2023-09-20 20:16:44 -0300

    Camadas

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -14,6 +14,8 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
+
+
         EntityManagerFactory factory = Persistence.createEntityManagerFactory( "oracle", getProperties() );
         EntityManager manager = factory.createEntityManager();
 
